acab01bf092cde4e87457c3956aa207d7cf574e0
Flávio Meira <flavio.meira@luizalabs.com>
2025-03-27 02:09:06 -0300

    adição: implementa integração com Firebase e exporta serviços de Firestore e Auth

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID",
+};
+
+// Inicializa o app
+const app = initializeApp(firebaseConfig);
+
+// Exporta os serviços que for usar
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
