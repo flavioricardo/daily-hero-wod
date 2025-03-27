e767019826fa8b34c055f6cd300276de00547eff
Flávio Meira <flavio.meira@luizalabs.com>
2025-03-27 02:04:44 -0300

    adição: atualiza caminho do manifest no index.html e define base no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/daily-hero-wod/",
   plugins: [
     react(),
     VitePWA({
